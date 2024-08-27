9137d46410e765fd9169421526f07b19de34d29f
oanapopescu93 <oanapopescu93@gmail.com>
2024-08-26 21:01:42 -0700

    modify .env

--- server/var/constants.js
+++ server/var/constants.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    AUTH_USER: "09f009efe7b9ae",
-    AUTH_PASS: "cd1233a2d8d6a7",
+    AUTH_USER: process.env.AUTH_USER,
+    AUTH_PASS: process.env.AUTH_PASS,
     AUTH_FROM: "oanapopescu93@gmail.com",
 
     SECRET_KEY: 'vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3',
